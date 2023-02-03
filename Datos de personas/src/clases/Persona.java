52f959bbb56abc82a333425fcdff2f7c36f8c06c
jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2023-02-03 14:33:32 +0100

    creo el repositorio y creo la clase persona

---
+++ Datos de personas/src/clases/Persona.java
@@ -0,0 +1,28 @@
+package clases;
+
+public class Persona {
+  private String nombre;
+  private String apellido;
+  private int edad;
+  public String getNombre() {
+    return nombre;
+  }
+  public String getApellido() {
+    return apellido;
+  }
+  public int getEdad() {
+    return edad;
+  }
+  public Persona(String nombre, String apellido, int edad) {
+    super();
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+  
+  public void cumplanios() {
+    edad=getEdad()+1;
+    System.out.println("Edad: " +edad);
+  }
+
+}
